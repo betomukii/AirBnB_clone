fb4abe4f2a41992232b11de9d1c98e92d1ab9dd8
amateurManu <oderoemmanuel6@gmail.com>
2024-03-10 07:42:40 -0700

    editted to retrieve all instances

--- console.py
+++ console.py
@@ -4,6 +4,12 @@ import cmd
 from models import storage
 from models.base_model import BaseModel
 from models import l_classes
+from models.user import User
+from models.place import Place
+from models.city import City
+from models.amenity import Amenity
+from models.state import State
+from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
@@ -27,7 +33,10 @@ class HBNBCommand(cmd.Cmd):
         elif type_model not in l_classes:
             print("** class doesn't exist **")
         else:
-            my_model = BaseModel()
+            dct = {'BaseModel': BaseModel, 'User': User, 'Place': Place,
+                   'City': City, 'Amenity': Amenity, 'State': State,
+                   'Review': Review}
+            my_model = dct[type_model]()
             print(my_model.id)
             my_model.save()
 
