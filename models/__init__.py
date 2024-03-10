a648d6417ea60cd000451000579977b4333c3c43
amateurManu <oderoemmanuel6@gmail.com>
2024-03-10 06:24:46 -0700

    added public class attributes

--- models/__init__.py
+++ models/__init__.py
@@ -6,3 +6,6 @@ from models.engine.file_storage import FileStorage
 
 storage = FileStorage()
 storage.reload()
+
+l_classes = ['BaseModel', 'User', 'Amenity',
+             'Place', 'City', 'State', 'Review']
